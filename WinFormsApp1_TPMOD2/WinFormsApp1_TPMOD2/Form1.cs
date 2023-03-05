a4d50191f69ecddd104459aa433ca0bd80d6d2f6
tangguhlaks <tangguhlaksana0@gmail.com>
2023-03-05 07:50:23 +0700

    add gui program

---
+++ WinFormsApp1_TPMOD2/WinFormsApp1_TPMOD2/Form1.cs
@@ -0,0 +1,31 @@
+namespace WinFormsApp1_TPMOD2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = input.Text;
+            label1.Text = "Halo "+text;
+        }
+    }
+}
